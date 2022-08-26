a25c70ae333ce469773bd7737757c55272f5d55f
prateekjain4 <56712397+prateekjain4@users.noreply.github.com>
2022-08-26 20:17:05 +0530

    Add files via upload

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/campTime')
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+})
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*20) + 10;
+        const camp = new Campground({
+            author: '623380edcf449b89f7ae1b31',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate, perspiciatis similique cupiditate eos eius et alias, placeat amet quaerat possimus nemo minima praesentium consectetur! Obcaecati nulla fugiat commodi aperiam?',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})  
